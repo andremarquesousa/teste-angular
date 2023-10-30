2752d955cc59f8e6f2dcfc61c7b83b2b1854498b
André Marques <andremarquesweb@gmail.com>
2023-10-30 16:59:21 -0300

    calc IR

--- src/app/common/components/simulator/simulator.component.ts
+++ src/app/common/components/simulator/simulator.component.ts
@@ -11,7 +11,7 @@ export class SimulatorComponent {
   @Output() value = new EventEmitter<any>();
   timeOut:any;
   simulatorForm!: FormGroup;
-  monthsList = [{ value: '', name: 'Selecione meses' }, { value: '2', name: '2 meses' }, { value: '3', name: '3 meses' }, { value: '4', name: '4 meses' }, { value: '5', name: '5 meses' }, { value: '6', name: '6 meses' }, { value: '12', name: '12 meses' }, { value: '24', name: '8 meses' }, { value: '36', name: '36 meses' }];
+  monthsList = [{ value: '', name: 'Selecione meses' }, { value: '2', name: '2 meses' }, { value: '3', name: '3 meses' }, { value: '4', name: '4 meses' }, { value: '5', name: '5 meses' }, { value: '6', name: '6 meses' }, { value: '12', name: '12 meses' }, { value: '24', name: '24 meses' }, { value: '36', name: '36 meses' }];
   result:number = 0;
   tax = 0;
 
@@ -26,9 +26,21 @@ export class SimulatorComponent {
     });
   }
 
+  IRValue(value:number, months:number) {
+    if (months <= 6) {
+      return value * (22.5 / 100) / 12;
+    } else if (months > 6 && months <= 12) {
+      return value * (20 / 100) / 12;
+    } else if (months > 12 && months <= 24) {
+      return value * (17.5 / 100) / 12;
+    } else {
+      return value * (15 / 100) / 12;
+    }
+  }
+
   handleSimulator() {
     let lastValue:number = this.simulatorForm.value.amount;
-    this.tax = lastValue * (22.5 / 100) / 12;
+    this.tax = this.IRValue(lastValue, Number(this.simulatorForm.value.months));
 
     const numberMonths = new Array(Number(this.simulatorForm.value.months)).fill(0);
 
